c06551de36dd8ef031fee840edb54083ad9cb623
roman01la <roman01la@romanliutikov.com>
2014-04-05 21:37:06 +0300

    remove deprecated Heroku deployment task

--- Gruntfile.js
+++ Gruntfile.js
@@ -135,16 +135,7 @@ module.exports = function (grunt) {
           ]
         }]
       },
-      heroku: {
-        files: [{
-          dot: true,
-          src: [
-            'heroku/*',
-            '!heroku/.git*',
-            '!heroku/Procfile'
-          ]
-        }]
-      },
+
       server: {
         dot: true,
         src: [
@@ -619,11 +610,6 @@ module.exports = function (grunt) {
     'rev',
     'usemin'
   ]);
-
-  grunt.registerTask('heroku', function () {
-    grunt.log.warn('The `heroku` task has been deprecated. Use `grunt build` to build for deployment.');
-    grunt.task.run(['build']);
-  });
 
   grunt.registerTask('default', [
     'newer:jshint',
